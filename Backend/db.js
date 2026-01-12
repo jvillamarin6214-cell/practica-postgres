ff4871e057e1855a8f5bf1d3e5d4fbc5024df66e
Jordan26Villamarin <johanvillamarin26@gmail.com>
2026-01-12 00:15:11 -0500

    Backend listo para Render

--- Backend/db.js
+++ Backend/db.js
@@ -2,9 +2,7 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  host: 'aws-0-us-west-2.pooler.supabase.com',
-  port: 6543,
-  database: 'postgres',
-  user: 'postgres.mavkevrfbfnorzcbowiv',
-  password: 'Practica2025'
-});
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Requerido para conectar con Render
+  }});
